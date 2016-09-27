94afac864b11f7f237d5376c6f7693da61454483
Alan Simplicio do Espirito Santo (Brq) <M105035@5800BSEGUROS.COM.BR>
2016-09-27 17:45:56 -0300

    Criação de sessão. *Com problema, corrigir em casa

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var load = require('express-load');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var expressSession = require('express-session');
 var app = express();
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+app.use(cookieParser('ntalk'));
+app.use(expressSession({resave: true,
+                saveUninitialized: true,
+                secret: true}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 
 load('models')
