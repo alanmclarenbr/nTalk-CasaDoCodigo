d522bae875104ba727e8e975dd2d857935976384
alanmclarenbr <alan.ses@gmail.com>
2016-10-04 18:44:54 -0300

    Correções de configuração do middleware Method-Override

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(expressSession({
     }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(methodOverride());
+app.use(methodOverride('_method'));
 app.use(express.static(__dirname + '/public'));
 
 load('models')
