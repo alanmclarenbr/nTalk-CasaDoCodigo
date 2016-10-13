e8db66334a86ff8d6fcf8e213d77c9d2ab46d021
Alan <alan.ses@gmail.com>
2016-10-12 23:10:37 -0300

    Adicionado chat one-to-one

--- sockets/chat.js
+++ sockets/chat.js
@@ -1,12 +1,26 @@
 module.exports = function(io){
-	io.sockets.on('connection', function(client){
-		console.log("io.sockets.on");
+	var crypto = require('crypto');
+    io.sockets.on('connection', function(client){
 		var session = client.handshake.session;
 		var usuario = session.usuario;
 		client.on('send-server', function(msg){
-			msg = "<b>"+usuario.nome+":</b> "+msg+"<br/>";
-			client.emit('send-client', msg);
-			client.broadcast.emit('send-client', msg);
+			var sala = session.sala;
+            var data = {email: usuario.email, sala: sala};
+		    msg = "<b>"+usuario.nome+":</b> "+msg+"<br/>";
+			client.broadcast.emit('new-message', data);
+            io.sockets.in(sala).emit('send-client', msg);
 		});
-	});
+        client.on('join', function (sala) {
+            if(!sala){
+                var timestamp = new Date().toString();
+                var md5 = crypto.createHash('md5');
+                sala = md5.update(timestamp).digest('hex');
+            }
+            session.sala = sala;
+            client.join(sala);
+        });
+        client.on('disconnect', function(){
+            client.leave(session.sala);
+        });
+    });
 }
