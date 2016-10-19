d8f84ecda469b82dad2ddc77a136b1dbe808313f
Alan <alan.ses@gmail.com>
2016-10-19 01:29:29 -0200

    Configuração do MongoDb e Redis em andamento

--- sockets/chat.js
+++ sockets/chat.js
@@ -1,8 +1,18 @@
 module.exports = function(io){
 	var crypto = require('crypto');
-    io.sockets.on('connection', function(client){
+    var sockets = io.sockets;
+    var online = {};
+
+    sockets.on('connection', function(client){
 		var session = client.handshake.session;
 		var usuario = session.usuario;
+
+        online[usuario.email] = usuario.email;
+        for(var email in online){
+            client.emit('notify-online', email);
+            client.broadcast.emit('notify-online', email);
+        }
+
 		client.on('send-server', function(msg){
 			var sala = session.sala;
             var data = {email: usuario.email, sala: sala};
@@ -20,6 +30,11 @@ module.exports = function(io){
             client.join(sala);
         });
         client.on('disconnect', function(){
+            var sala = session.sala;
+            var msg = "<b>"+ usuario.nome + ": </b> saiu.<br>";
+            client.broadcast.emit('notify-offline', usuario.email);
+            sockets.in(sala).emit('send-client', msg);
+            delete online[usuario.email];
             client.leave(session.sala);
         });
     });
